26032b2dd51312a631602323127a0c7002a0c32d
meibous <kuleshov995@yandex.ru>
2025-10-21 15:47:50 +0300

    KS-1-4 решение 4 задачи

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val length: String = "40_868_600_000";
+    val age: Byte = 27;
+    val partOfDay: Double = 0.075;
+    val seconds: Short = 6480;
+    val partOfYear: Float = 2.0547945E-4F;
+    val apogee: Int = 327000;
+
+    println("Расстояние $length")
+    println("Возраст $age")
+    println("Часть дня $partOfDay")
+    println("Секунды $seconds")
+    println("Часть года $partOfYear")
+    println("Апогей орбиты $apogee")
+}

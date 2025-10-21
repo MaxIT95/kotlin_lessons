066c7ce7b43aa24976ebf63079d62d5e5a4efe5c
meibous <kuleshov995@yandex.ru>
2025-10-21 15:43:32 +0300

    KS-1-3 удалил лишние файлы

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++
@@ -1,9 +0,0 @@
-package org.example.lesson_1
-
-fun main() {
-
-    // кол-во покупок, var т.к. изменяемая
-    var orderCount: Int = 0
-    // благодарность, делаем либо val либо const на уровне класса
-    val gratitudeMessage: String = "Thank for you purchase!"
-}

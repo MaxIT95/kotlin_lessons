3b51b392fafd23aab7a7350d727e2a9f66f46ca9
meibous <kuleshov995@yandex.ru>
2025-10-21 22:04:44 +0300

    KS-2-1 правки по замечаниям

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,15 +2,6 @@ package lesson_2
 
 fun main() {
 
-    /*Задача на среднее арифметическое. В классе с углубленным изучением
-     английского языка учится 4 человека. У них следующие баллы
-     по профильному предмету 3, 4, 3, 5.
-     Пишем часть школьного софта, который посчитает
-     средний балл по английскому для этого класса.
-
-  – Написать выражение, которое высчитывает средний балл;
-  – Распечатать результат в консоль;
-  – В консоли должно быть выведено дробное число с 2 знаками после запятой.*/
     val scores: List<Int> = listOf(3, 4, 3, 5)
 
     val averageScores = calculateAverage(scores)
@@ -18,13 +9,6 @@ fun main() {
     println("Средннее арифметическое $averageScores")
 }
 
-
 fun calculateAverage(scores: List<Int>): Float {
-
-    var sumValue: Float = 0F
-
-    scores.forEach {
-        sumValue = sumValue.plus(it)
-    }
-    return (sumValue / scores.size)
+    return (scores.sum().toFloat() / scores.size)
 }

8690c03fe8b425d81007652c5e8c5243d0bdee62
meibous <kuleshov995@yandex.ru>
2025-10-22 13:36:44 +0300

    KS-2-2 исправление замечаний

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -8,14 +8,9 @@ fun main() {
     val countIntern: Int = 30
     val salaryIntern: Int = 20000
 
-    // Расходы на выплату зарплаты постоянных сотрудников;
     val salaryConstantWorkers: Int = countWorkers * salaryWorker
     val salaryInterns: Int =  countIntern * salaryIntern
-    // Общие расходы по ЗП после прихода стажеров;
     val salaryWorkersAndInterns: Int = salaryConstantWorkers.plus(salaryInterns)
-
-    // Средняя ЗП одного сотрудника после устройства стажеров.
-
     val averageSalaryAfterInternsComing: Int = salaryWorkersAndInterns / (countWorkers + countIntern)
 
     println("Расходы на выплату зарплаты постоянных сотрудников: $salaryConstantWorkers")

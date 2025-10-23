17d3c196c412c0b4f693c7081eb20e6dc3e7451d
meibous <kuleshov995@yandex.ru>
2025-10-23 16:28:12 +0300

    KS-1-5 правки по замечаниям

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,9 @@
-package org.example.lesson_1
-
-import kotlin.math.min
+package lesson_1
 
 // не смог придумать адекватное название, поэтому сделал две
 const val secondsInOneMinute: Int = 60
 const val minutesInOneHour: Int = 60
-
+const val LOG_TIME_TEMPLATE: String = "%02d"
 const val TEMPLATE_LOG: String = "Время, проведенное в космосе %s:%s:%s"
 
 fun main() {
@@ -15,29 +13,17 @@ fun main() {
     val minutes = seconds / secondsInOneMinute
 
     val hours: Int = minutes / minutesInOneHour
-    val hoursAsString: String = formatIntAsString(hours)
+    val hoursAsString: String = LOG_TIME_TEMPLATE.format(hours)
 
     val minRemains: Int = minutes % minutesInOneHour
-    val minRemainsAsString: String? = formatIntAsString(minRemains)
+    val minRemainsAsString: String? = LOG_TIME_TEMPLATE.format(minRemains)
 
     val secRemains: Int = seconds % ((hours * minutesInOneHour * secondsInOneMinute) + (minRemains * 60))
-    val secRemainsAsString: String = formatIntAsString(secRemains)
+    val secRemainsAsString: String = LOG_TIME_TEMPLATE.format(secRemains)
 
     println(
-        "Время, проведенное в космосе ${
-            String.format(
-                TEMPLATE_LOG,
-                hoursAsString, minRemainsAsString, secRemainsAsString
-            )
-        }"
+        TEMPLATE_LOG.format(
+            hoursAsString, minRemainsAsString, secRemainsAsString
+        )
     )
-}
-
-fun formatIntAsString(someInt: Int): String {
-
-    return if (someInt.toString().length == 1) {
-        "0$someInt"
-    } else {
-        someInt.toString()
-    }
 }

53c66430cbba6f6989e42e26731c4b471088051f
meibous <kuleshov995@yandex.ru>
2025-10-23 16:57:23 +0300

    KS-3-1 решение задачи 1

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,17 @@
+package lesson3
+
+const val GREETING_DAY: String = "Добрый день %s"
+const val GREETING_EVENING: String = "Добрый вечер %s"
+
+fun main() {
+
+    val userName: String = "Юрий"
+    var greetingMessage: String? = null
+
+    greetingMessage = GREETING_DAY.format(userName)
+    println(greetingMessage)
+
+    greetingMessage = GREETING_EVENING.format(userName)
+    println(greetingMessage)
+
+}

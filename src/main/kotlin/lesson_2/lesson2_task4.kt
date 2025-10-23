512744b14cee6c76af689cfc05577f5f170adfae
meibous <kuleshov995@yandex.ru>
2025-10-23 16:33:52 +0300

    KS-2-4 правки по замечаниям

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,12 +1,15 @@
-package org.example.lesson_2
+package lesson_2
+
+const val BONUS_PERCENT: Int = 20
+const val ALL_PERCENT: Int = 100
 
 fun main() {
 
     val crystalMass: Int = 7
     val ironMass: Int = 11
 
-    val bonusCrystal: Int = crystalMass.times(0.2).toInt()
-    val bonusIron: Int = ironMass.times(0.2).toInt()
+    val bonusCrystal: Int = crystalMass * BONUS_PERCENT / ALL_PERCENT
+    val bonusIron: Int = ironMass * BONUS_PERCENT / ALL_PERCENT
 
     println("Бонусная кристаллическая руда $bonusCrystal")
     println("Бонусная железная руда $bonusIron")

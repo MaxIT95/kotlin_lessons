83d3a5f3a9b0658183a5a3de195d4ad217d59a26
meibous <kuleshov995@yandex.ru>
2025-10-23 10:01:19 +0300

    KS-2-3 правки по замечаниям

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,14 +4,22 @@ fun main() {
 
     val departureHour: Int = 9
     val departureMinutes: Int = 39
-//    val travelMinutes: Int = 457
-    val travelMinutes: Int = 445
+    val travelMinutes: Int = 457
     val countMinutesOrSeconds: Int = 60
 
     val minutes: Int = departureMinutes + (departureHour * countMinutesOrSeconds)
 
     val minutesArrival = minutes + travelMinutes
 
-    println("Час прибытия ${minutesArrival / countMinutesOrSeconds}")
-    println("Минуты прибытия ${minutesArrival % countMinutesOrSeconds}")
+    println("Час прибытия ${formatIntAsString(minutesArrival / countMinutesOrSeconds)}")
+    println("Минуты прибытия ${formatIntAsString(minutesArrival % countMinutesOrSeconds)}")
+}
+
+fun formatIntAsString(someInt: Int): String {
+
+    return if (someInt.toString().length == 1) {
+        "0$someInt"
+    } else {
+        someInt.toString()
+    }
 }

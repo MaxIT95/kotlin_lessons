da0a80a9cca4f25ee42f2b561daa32ff30e45721
meibous <kuleshov995@yandex.ru>
2025-10-23 09:45:17 +0300

    KS-1-5 правки по замечаниям

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,22 +2,35 @@ package org.example.lesson_1
 
 import kotlin.math.min
 
+// не смог придумать адекватное название, поэтому сделал две
+const val secondsInOneMinute: Int = 60
+const val minutesInOneHour: Int = 60
+
+const val TEMPLATE_LOG: String = "Время, проведенное в космосе %s:%s:%s"
+
 fun main() {
 
     val seconds: Int = 6490
 
-    val minutes = seconds / 60
+    val minutes = seconds / secondsInOneMinute
 
-    val hours: Int = minutes / 60
+    val hours: Int = minutes / minutesInOneHour
     val hoursAsString: String = formatIntAsString(hours)
 
-    val minRemains: Int = minutes % 60
+    val minRemains: Int = minutes % minutesInOneHour
     val minRemainsAsString: String? = formatIntAsString(minRemains)
 
-    val secRemains: Int = seconds - (hours * 60 * 60) - (minRemains * 60)
+    val secRemains: Int = seconds % ((hours * minutesInOneHour * secondsInOneMinute) + (minRemains * 60))
     val secRemainsAsString: String = formatIntAsString(secRemains)
 
-    println("Время, проведенное в космосе $hoursAsString:$minRemainsAsString:$secRemainsAsString")
+    println(
+        "Время, проведенное в космосе ${
+            String.format(
+                TEMPLATE_LOG,
+                hoursAsString, minRemainsAsString, secRemainsAsString
+            )
+        }"
+    )
 }
 
 fun formatIntAsString(someInt: Int): String {

74426042a18d166689cb832cc66683722215e8f8
meibous <kuleshov995@yandex.ru>
2025-10-24 11:24:16 +0300

    KS-3-3 решение задачи

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -2,9 +2,12 @@ package lesson_3
 
 fun main() {
 
-    val originNumber: Int = 3
+    val originNumber: Int = 8
+    var multiplicationTable = ""
 
     for (i in 1..9) {
-        println("$originNumber x $i = ${originNumber * i}")
+        multiplicationTable = "$multiplicationTable$originNumber x $i = ${originNumber * i}\n"
     }
+
+    println(multiplicationTable)
 }

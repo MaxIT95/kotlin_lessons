3722523efb50d26ca9e4cce485c6bd8236a36a1f
meibous <kuleshov995@yandex.ru>
2025-10-24 11:32:53 +0300

    KS-3-4 решение задачи

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var strokeNumber = 1
+
+    println("$from-$to;$strokeNumber")
+
+    from = "D2"
+    to = "D3"
+    strokeNumber = 2
+
+    println("$from-$to;$strokeNumber")
+}

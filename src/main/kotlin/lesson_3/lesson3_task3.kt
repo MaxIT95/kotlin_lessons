43dc13128a19b10ea4cb06f897588ac73d4ea6d4
meibous <kuleshov995@yandex.ru>
2025-10-24 15:51:27 +0300

    KS-3-3 исправление замечаний

--- src/main/kotlin/lesson_3/lesson3_task3.kt
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -2,12 +2,18 @@ package lesson_3
 
 fun main() {
 
-    val originNumber: Int = 8
-    var multiplicationTable = ""
+    val originNumber = 8
 
-    for (i in 1..9) {
-        multiplicationTable = "$multiplicationTable$originNumber x $i = ${originNumber * i}\n"
-    }
+    val multiplicationTable =
+        """|$originNumber x 1 = ${originNumber * 1}
+            |$originNumber x 2 = ${originNumber * 2}
+            |$originNumber x 3 = ${originNumber * 3}
+            |$originNumber x 4 = ${originNumber * 4}
+            |$originNumber x 5 = ${originNumber * 5}
+            |$originNumber x 6 = ${originNumber * 6}
+            |$originNumber x 7 = ${originNumber * 7}
+            |$originNumber x 8 = ${originNumber * 8}
+            |$originNumber x 9 = ${originNumber * 9}""".trimMargin()
 
     println(multiplicationTable)
 }

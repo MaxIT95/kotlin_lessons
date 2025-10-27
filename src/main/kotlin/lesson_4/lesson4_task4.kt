80100cdefdc41f4f44d5e58a563670d326d088a6
meibous <kuleshov995@yandex.ru>
2025-10-27 21:42:32 +0300

    KS-4-4 решение задачи

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -5,9 +5,9 @@ fun main() {
     val currentTrainingDay = 5
 
     println(
-        """Упражнения для рук: ${currentTrainingDay % 2 != 0}
-Упражнения для ног: ${currentTrainingDay % 2 == 0}
-Упражнения для спины:  ${currentTrainingDay % 2 == 0}
-Упражнения для пресса: ${currentTrainingDay % 2 != 0}""".trimIndent()
+        """|Упражнения для рук: ${currentTrainingDay % 2 != 0}                 
+                   |Упражнения для ног: ${currentTrainingDay % 2 == 0}
+|Упражнения для спины:  ${currentTrainingDay % 2 == 0}
+|Упражнения для пресса: ${currentTrainingDay % 2 != 0}""".trimMargin()
     )
 }

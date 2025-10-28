1cb75eba7e0f5987a9a9b40095663d93265f5ed4
meibous <kuleshov995@yandex.ru>
2025-10-28 21:20:10 +0300

    KS-4-5 правка замечаний

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,16 +1,23 @@
 package lesson_4
 
+const val MIN_WORKERS = 55
+const val MAX_WORKERS = 70
+const val MIN_BOX_FOODS = 50
+const val SHIP_NOT_BROKEN = false
+const val SHIP_IS_BROKEN = true
+const val WEATHER_IS_GOOD = true
+
 fun main() {
 
     val isShipBroken: Boolean = readLine().toBoolean()
-    val workersCount: Int = readLine()?.toInt() ?: 0
-    val boxFoodCount: Int = readLine()?.toInt() ?: 0
+    val workersCount: Int = readln().toInt()
+    val boxFoodCount: Int = readln().toInt()
     val weatherIsGood: Boolean = readLine().toBoolean()
 
     val isShipSetSail =
-        (!isShipBroken && 55 <= workersCount && workersCount <= 70 && 50 < boxFoodCount)
-                || (isShipBroken && workersCount == 70
-                && 50 <= boxFoodCount && weatherIsGood)
+        (isShipBroken == SHIP_NOT_BROKEN && MIN_WORKERS <= workersCount && workersCount <= MAX_WORKERS && 50 < MIN_BOX_FOODS)
+                || (isShipBroken == SHIP_IS_BROKEN && workersCount == MAX_WORKERS
+                && MIN_BOX_FOODS <= boxFoodCount && weatherIsGood == WEATHER_IS_GOOD)
 
     println("Корабль может отправиться в плавание: $isShipSetSail")
 }

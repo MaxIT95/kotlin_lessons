8a32013744c2fbcc8b439860e90ca389f9de16e6
meibous <kuleshov995@yandex.ru>
2025-11-01 11:21:03 +0300

    KS-7-2 решение задачи

---
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -0,0 +1,25 @@
+package lesson_7
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+fun main() {
+
+    val range = 1000..9999
+
+    var codeAuthorization: Int?
+
+    while (true) {
+        codeAuthorization = Random.nextInt(range)
+        println("Ваш код авторизации: $codeAuthorization")
+        println("Введите код авторизации")
+        val codeFromUser = readln()
+
+        if (codeFromUser == codeAuthorization.toString()) {
+            println("Добрый день!")
+            break
+        } else {
+            println("Пароль введен неправильно!")
+        }
+    }
+}

69af5f9d3d1ce130dfbcc3d4b61bebaebaaaec3b
meibous <kuleshov995@yandex.ru>
2025-11-04 13:21:58 +0300

    KS-8-1 правки

--- src/main/kotlin/lesson_8/lesson8_task1.kt
+++ src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -3,11 +3,7 @@ package lesson_8
 fun main() {
 
     val countViews = arrayOf(4, 2, 13, 5, 6, 29, 8)
+    val sumViews = countViews.sum()
 
-    var sumViews = 0
-
-    countViews.forEach({
-        sumViews = sumViews + it
-    })
     println("Кол-во просмотров за неделю: $sumViews")
 }

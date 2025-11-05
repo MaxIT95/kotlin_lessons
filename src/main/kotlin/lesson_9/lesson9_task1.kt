f57c8303f537738f40a64ee6393ee04c448f53e8
meibous <kuleshov995@yandex.ru>
2025-11-05 11:42:11 +0300

    KS-9-1 правки

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -2,7 +2,7 @@ package lesson_9
 
 fun main() {
 
-    val ingredients = mutableListOf("огурец", "помидор", "картошка")
+    val ingredients = listOf("огурец", "помидор", "картошка")
     println(
         "В рецепте есть следующие ингридиенты: \n${ingredients.joinToString("\n")}"
     )

f0c841e0a697f3ef1144e74e5b17307055b458a7
meibous <kuleshov995@yandex.ru>
2025-11-05 11:40:32 +0300

    KS-9-5 правки

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -2,23 +2,18 @@ package lesson_9
 
 fun main() {
 
-    var ingredient: String
-    val ingredientList = mutableListOf<String>()
+    val ingredientSet = mutableSetOf<String>()
 
     for (i in 1..5) {
-
-        while (true) {
-            println("Введите название ингридиента:")
-            ingredient = readln()
-
-            if (!ingredientList.contains(ingredient)) {
-                ingredientList.add(ingredient)
-                break
-            }
-        }
+        println("Введите название ингридиента:")
+        ingredientSet.add(readln())
     }
-    ingredientList.sort()
-    ingredientList[0] = ingredientList[0].substring(0, 1).uppercase() + ingredientList[0].substring(1)
+    val sortedSet = ingredientSet.toSortedSet()
 
-    println("Ингридиенты: ${ingredientList.joinToString(", ")}")
+    val firstProduct = sortedSet.elementAt(0)
+
+    sortedSet.remove(firstProduct)
+    sortedSet.add(firstProduct.replaceFirstChar { it.uppercase() })
+
+    println("Ингридиенты: ${sortedSet.joinToString(", ")}")
 }

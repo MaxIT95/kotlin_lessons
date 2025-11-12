dfe383734047a83b2d479aca4f06dcc713e99a96
meibous <kuleshov995@yandex.ru>
2025-11-12 20:53:19 +0300

    KS-11-4 добавил поля

--- src/main/kotlin/lesson11_task4.kt
+++ src/main/kotlin/lesson11_task4.kt
@@ -1,5 +1,5 @@
-data class CategoryReceipt(val title: String, val receipts: List<Receipt>)
+data class CategoryReceipt(val title: String, val pictureSource: String, val receipts: List<Receipt>)
 
-data class Receipt(val ingredients: List<Ingredients>)
+data class Receipt(val name: String, val pictureSource: String, val portion: Integer, val ingredients: List<Ingredients>)
 
 data class Ingredients(val name: String, val quantity: Integer)

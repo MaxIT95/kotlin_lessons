6e554449bf028afa5d23c709eb26cc1d0bed489d
meibous <kuleshov995@yandex.ru>
2025-11-15 00:02:15 +0300

    KS-12-4 правки

--- src/main/kotlin/lesson_12/lesson12_task4.kt
+++ src/main/kotlin/lesson_12/lesson12_task4.kt
@@ -1,24 +1,17 @@
 package lesson_12
 
+const val ABSOLUTE_ZERO = 273.15
+
 fun main() {
 
     val firstWeatherInfo = DayWeatherDto(7, 3, true)
 }
 
-class DayWeatherDto {
+class DayWeatherDto(var dayTemperature: Int, var nightTemperature: Int, var isPrecipitation: Boolean) {
 
-    var dayTemperature: Int
-    var nightTemperature: Int
-    var isPrecipitation: Boolean
-
-    companion object Constants {
-        const val ABSOLUTE_ZERO = 273.15
-    }
-
-    constructor(_dayTemperature: Int, _nightTemperature: Int, _isPrecipitation: Boolean) {
-        dayTemperature = (_dayTemperature - ABSOLUTE_ZERO).toInt()
-        nightTemperature = (_nightTemperature - ABSOLUTE_ZERO).toInt()
-        isPrecipitation = _isPrecipitation
+    init {
+        this.dayTemperature = (dayTemperature - ABSOLUTE_ZERO).toInt()
+        this.nightTemperature = (nightTemperature - ABSOLUTE_ZERO).toInt()
         println(toString())
     }
 

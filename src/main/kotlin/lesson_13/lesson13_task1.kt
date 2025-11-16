b5e0fcc34e1a452f2382da1d7ed24cd32d0b8d39
meibous <kuleshov995@yandex.ru>
2025-11-16 12:28:05 +0300

    KS-13-1 решение задачи

---
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -0,0 +1,8 @@
+package lesson_13
+
+fun main() {
+    val comp = TelephoneDictionary("test", 123L, null)
+}
+
+class TelephoneDictionary(val name: String, val telephoneNumber: Long, val company: String?)
+

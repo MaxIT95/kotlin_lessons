6a653411064381938e16b6d5d20d4e3c4c7d29cc
meibous <kuleshov995@yandex.ru>
2025-11-18 15:06:01 +0300

    KS-13-5 решение

---
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -0,0 +1,67 @@
+package lesson_13
+
+const val YES = "да"
+
+fun main() {
+
+    val contacts = ContactDictionaryDto()
+
+    var name: String
+    var phone: Long
+    var company: String?
+
+    while (true) {
+        println("введите имя контакта")
+        name = readln()
+        println("введите номер телефона")
+        try {
+            phone = readln().toLong()
+        } catch (ex: NumberFormatException) {
+            println("Ошибка $ex")
+            continue
+        }
+        phone = readln().toLong()
+        println("введите название компании")
+        company = readln()
+
+        if (company.isEmpty()) {
+            contacts.addContact(PhoneContact(name, phone, null))
+        } else {
+            contacts.addContact(PhoneContact(name, phone, company))
+        }
+        println("Добавить еще один контакт? да/нет")
+        if (readln().equals(YES, true)) {
+            continue
+        }
+        break
+    }
+    println("Контакты: $contacts")
+
+}
+
+class ContactDictionaryDto {
+
+    val listContact = mutableListOf<PhoneContact>()
+
+    fun addContact(contact: PhoneContact) {
+        if (contact.telephoneNumber == null) {
+            println("Номер телефона обязательное поле!")
+        } else {
+            listContact.add(contact)
+        }
+    }
+
+    override fun toString(): String {
+        return listContact.toString()
+    }
+}
+
+
+class PhoneContact(val name: String, val telephoneNumber: Long?, val company: String? = null) {
+
+    override fun toString(): String {
+        return "- Имя: $name\n" +
+                "- Номер: $telephoneNumber\n" +
+                "- Компания: ${company ?: "<не указано>"}"
+    }
+}

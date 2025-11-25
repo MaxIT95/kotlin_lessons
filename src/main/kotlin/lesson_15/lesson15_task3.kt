48e59efe2c06706818a489751c02d6522f8097a1
kuleshow <kuleshov995@yandex.ru>
2025-11-25 20:22:10 +0300

    KS-15-3: правки

--- src/main/kotlin/lesson_15/lesson15_task3.kt
+++ src/main/kotlin/lesson_15/lesson15_task3.kt
@@ -2,28 +2,43 @@ package lesson_15
 
 fun main() {
     val user1 = User("Max")
-    val user2 = User("John")
     val admin = Admin("Kurt")
 
-    admin.addUser(user1)
-    admin.addUser(user2)
-    admin.removeUser(user1)
-}
-abstract class SimpleUser(val name: String)
-
-interface AdminAccessibility {
-    fun addUser(user: SimpleUser)
-    fun removeUser(user: SimpleUser)
+    val msgId = user1.writeMessage("Hello")
+    user1.readForum()
+    user1.readForum()
+    admin.deleteUser(user1)
+    admin.deleteMessage(msgId)
 }
 
-class User(name: String) : SimpleUser(name) {}
-class Admin(name: String) : SimpleUser(name), AdminAccessibility {
+abstract class SimpleUser(val name: String) {
 
-    override fun addUser(user: SimpleUser) {
-        println("Добавляю юзера: ${user.name}")
+    var messageId: Int = 0
+
+    fun readForum() {
+        println("$name readForum...")
     }
 
-    override fun removeUser(user: SimpleUser) {
-        println("Удаляю юзера: ${user.name}")
+    fun writeMessage(message: String): Int {
+        println("${name}: $message")
+        return messageId++
+    }
+}
+
+interface AdminAccessibility {
+    fun deleteMessage(messageId: Int)
+    fun deleteUser(user: SimpleUser)
+}
+
+class User(name: String) : SimpleUser(name)
+
+class Admin(name: String) : SimpleUser(name), AdminAccessibility {
+
+    override fun deleteMessage(messageId: Int) {
+        println("Remove message with id: $messageId")
+    }
+
+    override fun deleteUser(user: SimpleUser) {
+        println("Remove user: ${user.name}")
     }
 }

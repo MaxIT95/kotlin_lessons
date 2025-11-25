fad3d05e8ebfeb05443f92cc6f08a57c01effcac
kuleshow <kuleshov995@yandex.ru>
2025-11-25 21:18:35 +0300

    KS-15-5: решение

---
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -0,0 +1,91 @@
+package lesson_15
+
+fun main() {
+
+    val jeep = Jeep(3);
+
+    jeep.loadingPassengers(3)
+    jeep.ride()
+    jeep.unloadingCargo(3)
+    println("Jeep transported 3 passenger")
+
+    jeep.loadingPassengers(3)
+    jeep.ride()
+    jeep.unloadingCargo(3)
+    println("Jeep transported 3 passenger")
+
+    val cargoCar = CargoCar(1, 2)
+    cargoCar.loadingCargo(2)
+    cargoCar.ride()
+    cargoCar.unloadingCargo(2)
+
+    println("Cargo car transported 2 cargo")
+}
+
+interface PassengerTransporting {
+    fun loadingPassengers(passengers: Int)
+    fun unloadingPassengers(passengers: Int)
+}
+
+interface CargoTransporting {
+    fun loadingCargo(cargo: Int)
+    fun unloadingCargo(cargo: Int)
+}
+
+interface VehicleMoving {
+    fun ride() {
+        println("Transport is ride...")
+    }
+}
+
+abstract class Vehicle(var passengerCapacity: Int?, var cargoCapacity: Int?) : PassengerTransporting,
+    CargoTransporting, VehicleMoving {
+
+    var cargoCurrentCount = 0
+    var passengerCurrentCount = 0
+
+    override fun loadingPassengers(passengers: Int) {
+        if (passengerCapacity != null) {
+            if (passengerCurrentCount + passengers <= passengerCapacity!!) {
+                println("Loading passengers: $passengers")
+                this.passengerCurrentCount += passengers
+                println("Current passengers loaded: $passengerCurrentCount")
+            } else {
+                println("Transport is overloading by passengers!")
+            }
+        }
+    }
+
+    override fun unloadingPassengers(passengers: Int) {
+        if (passengerCapacity != null) {
+            println("Unloading cargo $passengers")
+            this.passengerCapacity = this.passengerCapacity!! - passengers
+            println("Current cargo loaded: $passengerCurrentCount")
+        }
+    }
+
+    override fun loadingCargo(cargo: Int) {
+        if (cargoCapacity != null) {
+            if (cargoCurrentCount + cargo <= cargoCapacity!!) {
+                println("Loading cargo $cargo")
+                this.cargoCurrentCount += cargo
+                println("Current cargo loaded: $cargoCurrentCount")
+            }
+        }
+    }
+
+    override fun unloadingCargo(cargo: Int) {
+        if (cargoCapacity != null) {
+            println("Unloading cargo $cargo")
+            this.cargoCapacity = this.cargoCapacity!! - cargo
+            println("Current cargo loaded: $passengerCurrentCount")
+        }
+    }
+
+}
+
+class CargoCar(
+    passengersCapacity: Int, cargoCapacity: Int
+) : Vehicle(passengersCapacity, cargoCapacity)
+
+class Jeep(passengers: Int) : Vehicle(passengers, null)

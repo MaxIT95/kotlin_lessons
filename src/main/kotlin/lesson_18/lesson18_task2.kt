e6d59f47d9125792d7de8457e2823974e6df920a
kuleshow <kuleshov995@yandex.ru>
2025-11-29 20:05:59 +0300

    KS-18-2: правки

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -1,21 +1,21 @@
 package lesson_18
 
 fun main() {
-    val bones1 = FourBones()
-    val bones2 = SixBones()
-    val bones3 = EightBones()
+    val bones1 = FourBones(4)
+    val bones2 = SixBones(6)
+    val bones3 = EightBones(8)
 
-    val listBones = arrayListOf<ThrowBones>(bones1, bones2, bones3)
+    val listBones = arrayListOf<Bones>(bones1, bones2, bones3)
 
     listBones.forEach { it.throwBones() }
 }
 
-interface ThrowBones {
+abstract class Bones(val count: Int) {
     fun throwBones() {
-        println((1..6).random())
+        println((1..count).random())
     }
 }
 
-class FourBones : ThrowBones
-class SixBones : ThrowBones
-class EightBones : ThrowBones
+class FourBones(count: Int) : Bones(count)
+class SixBones(count: Int) : Bones(count)
+class EightBones(count: Int) : Bones(count)

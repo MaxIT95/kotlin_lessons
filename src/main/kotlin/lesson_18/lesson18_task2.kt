a58f1875376c9e9bb623743c74ddae3ed70e3e3f
kuleshow <kuleshov995@yandex.ru>
2025-12-01 09:18:15 +0300

    KS-18-2: правки

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -1,9 +1,9 @@
 package lesson_18
 
 fun main() {
-    val bones1 = FourBones(4)
-    val bones2 = SixBones(6)
-    val bones3 = EightBones(8)
+    val bones1 = FourBones()
+    val bones2 = SixBones()
+    val bones3 = EightBones()
 
     val listBones = arrayListOf<Bones>(bones1, bones2, bones3)
 
@@ -16,6 +16,6 @@ abstract class Bones(val count: Int) {
     }
 }
 
-class FourBones(count: Int) : Bones(count)
-class SixBones(count: Int) : Bones(count)
-class EightBones(count: Int) : Bones(count)
+class FourBones : Bones(4)
+class SixBones : Bones(6)
+class EightBones : Bones(8)

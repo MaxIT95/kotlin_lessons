ae31e62fc1c7e7cae04074631859170fb1ba1c20
kuleshow <kuleshov995@yandex.ru>
2025-12-01 22:23:18 +0300

    KS-19-3: решение задачи

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,25 @@
+package lesson_19
+
+fun main() {
+
+    val spaceShip = SpaceShip()
+
+    spaceShip.flyUp()
+    spaceShip.fire()
+    spaceShip.flyDown()
+}
+
+class SpaceShip {
+
+    fun flyUp() {
+        println("Flying up")
+    }
+
+    fun flyDown() {
+        TODO()
+    }
+
+    fun fire() {
+        // TODO реализовать метод
+    }
+}

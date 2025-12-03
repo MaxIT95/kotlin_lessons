cff424db1cec201a733f010f19cc07fd03924cfc
kuleshow <kuleshov995@yandex.ru>
2025-12-03 17:49:02 +0300

    KS-20-4: решение задачи

---
+++ src/main/kotlin/lesson_20/lessson20_task4.kt
@@ -0,0 +1,14 @@
+package lesson_20
+
+fun main() {
+
+    val elements = listOf("кнопка 1", "кнопка 2", "кнопка 3", "кнопка 4", "кнопка 5", "кнопка 6")
+
+    val functions: List<() -> Unit> = elements.map { { println("Press button $it!") } }
+
+    for (i in 1..functions.size) {
+        if (i % 2 == 0) {
+            functions[i].invoke()
+        }
+    }
+}

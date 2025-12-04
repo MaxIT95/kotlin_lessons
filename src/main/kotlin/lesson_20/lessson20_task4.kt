829d415ca089f10f698f8e309c6d73185d0ceb8d
kuleshow <kuleshov995@yandex.ru>
2025-12-04 16:12:30 +0300

    KS-20-4: правки

--- src/main/kotlin/lesson_20/lessson20_task4.kt
+++ src/main/kotlin/lesson_20/lessson20_task4.kt
@@ -1,18 +1,12 @@
 package lesson_20
 
 fun main() {
-
     val elements = listOf("кнопка 1", "кнопка 2", "кнопка 3", "кнопка 4", "кнопка 5", "кнопка 6")
-
     val functions: List<() -> Unit> = elements.map { { println("Press button $it!") } }
 
     functions.forEachIndexed { index, function ->
-
-        if (index > 0) {
-
-            if (index % 2 != 0) {
-                function.invoke()
-            }
+        if ((index + 1) % 2 == 0) {
+            function.invoke()
         }
     }
 }

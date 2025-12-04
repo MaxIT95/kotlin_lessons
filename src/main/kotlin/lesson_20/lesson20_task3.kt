f7da1fefc522c44b948ae737ab43f56750750bda
kuleshow <kuleshov995@yandex.ru>
2025-12-04 12:15:37 +0300

    KS-20-3: правки

--- src/main/kotlin/lesson_20/lesson20_task3.kt
+++ src/main/kotlin/lesson_20/lesson20_task3.kt
@@ -4,15 +4,15 @@ fun main() {
     val gamer1 = Gamer(true)
     val gamer2 = Gamer(false)
 
-    val key: (Gamer) -> String = { player: Gamer ->
-        if (player.isHaveKey) {
+    val checkDoor: (Gamer) -> String = { player: Gamer ->
+        if (player.hasKey) {
             "Player open the door"
         } else {
             "Door is closed"
         }
     }
-    println(key(gamer1))
-    println(key(gamer2))
+    println(checkDoor(gamer1))
+    println(checkDoor(gamer2))
 }
 
-class Gamer(var isHaveKey: Boolean)
+class Gamer(var hasKey: Boolean)

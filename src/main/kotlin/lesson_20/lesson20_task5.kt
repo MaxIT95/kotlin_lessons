ac910760a32841390e4500e797ea7e1c4db25d97
kuleshow <kuleshov995@yandex.ru>
2025-12-06 13:18:58 +0300

    KS-20-5: правки

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -5,21 +5,21 @@ fun main() {
 
     robot.say()
 
-    var modifier: (String) -> String = { it.reversed() }
+    val modifier: (String) -> String = { it.reversed() }
     robot.setModifier(modifier)
     robot.say()
 }
 
 class Robot {
-    private var modifier: (String) -> String = { "$it" }
+    private var modifier: (String) -> String = { it }
 
-    val phrases = listOf("Hello boy", "Good boy!", "Your died..", "Have a nice day!", "Good luck boy...")
+    private val phrases = listOf("Hello boy", "Good boy!", "Your died..", "Have a nice day!", "Good luck boy...")
 
     fun setModifier(function: (String) -> String) {
         this.modifier = function
     }
 
     fun say() {
-        println(modifier.invoke(phrases.random()))
+        println(modifier(phrases.random()))
     }
 }
